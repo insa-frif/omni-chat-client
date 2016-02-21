1fd39985891c188b46b15a94439a0d38d8aa6222
Charles "Demurgos" Samborski <demurgos@demurgos.net>
2016-02-21 19:33:57 +0100

    Angular quickstart - step 2

--- src/browser/components/tuto-app/tuto-app.ts
+++ src/browser/components/tuto-app/tuto-app.ts
@@ -5,6 +5,19 @@ export interface Hero {
   name: string;
 }
 
+let HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
+
 @Component({
   selector: 'tuto-app',
   templateUrl: './components/tuto-app/tuto-app.html',
@@ -12,8 +25,10 @@ export interface Hero {
 })
 export class TutoApp {
   public title = 'Tour of Heroes';
-  public hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  public heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
