b27784620bf948caa956f3b1c019a8485ea95ea7
Sn0wFox <ruben.pericas@laposte.net>
2016-02-25 11:15:39 +0100

    Moved everything in oc-app

--- src/browser/components/oc-app/oc-app.component.ts
+++ src/browser/components/oc-app/oc-app.component.ts
@@ -2,19 +2,23 @@
  * Created by Ruben on 25/02/2016.
  */
 
-import {Component} from '../../../../node_modules/angular2/core.d';
+import {Component} from 'angular2/core';
+import {Type} from "angular2/core";
+
+import {SliderComponent} from '../oc-slider/oc-slider.component';
 
 @Component({
 	selector: "oc-app",
-	templateUrl: "./oc-app.component.html",
-	styleUrls: ["./oc-app.component.css"]
+	templateUrl: "./components/oc-app/oc-app.component.html",
+	styleUrls: ["./components/oc-app/oc-app.component.css"],
+	directives: [<Type>SliderComponent],
 })
 export class OcApp
 {
-	title: string = "My OmniChat client";
+	title: string = "Welcome you !";
 
 	constructor()
 	{
-		// TODO(Ruben) : fill it
+		// TODO(Ruben) : fill it when necessary
 	}
 }
--- src/browser/main.ts
+++ src/browser/main.ts
@@ -2,9 +2,10 @@ import 'reflect-metadata';
 import {Type} from "angular2/core";
 import {bootstrap} from 'angular2/platform/browser';
 
+import {OcApp} from './components/oc-app/oc-app.component';
 import {SliderComponent} from './components/oc-slider/oc-slider.component';
 
-bootstrap(<Type>SliderComponent);
+bootstrap(<Type>OcApp);
 
 /* LISTENERS */
 // TODO : deplacer ce qui suit dans un nouveau fichier
