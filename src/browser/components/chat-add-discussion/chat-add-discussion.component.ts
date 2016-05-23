86a1df105d2522b233699dffb7f259e42746343d
Sn0wFox <ruben.pericas@laposte.net>
2016-05-23 14:55:38 +0200

    Added component chat-add-discussion

---
+++ src/browser/components/chat-add-discussion/chat-add-discussion.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {MdCard} from '@angular2-material/card/card';
+import {MdIcon} from '@angular2-material/icon/icon';
+import {MdList, MdListItem} from '@angular2-material/list/list';
+import {DiscussionService} from "../../services/discussion.service";
+import {ObservableDiscussion} from "../../../core/observables/observable-discussion";
+
+@Component({
+  selector: "oc-chat-discussion",
+  templateUrl: "./components/chat-discussion/chat-discussion.component.html",
+  styleUrls: ["./components/chat-discussion/chat-discussion.component.css"],
+  directives: [MdCard, MdIcon, MdList, MdListItem]
+})
+export class ChatDiscussionComponent implements OnInit {
+  public discussion: ObservableDiscussion;
+
+  private _discussionService: DiscussionService;
+
+  constructor (discussionService: DiscussionService) {
+    this._discussionService = discussionService;
+  }
+
+  public ngOnInit(): void {
+    this._discussionService.currentDiscussion
+      .subscribe((discussion: ObservableDiscussion) => {
+        this.discussion = discussion;
+      });
+  }
+}
